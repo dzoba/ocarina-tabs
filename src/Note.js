480f83e86ce2dbadd384e81acd337d5eaf16f4d4
Chris Dzoba <chris@dzoba.com>
2017-03-09 22:59:45 -0800

    Use scientific pitch notation.

--- src/Note.js
+++ src/Note.js
@@ -1,63 +1,63 @@
 import React, { Component } from 'react';
 
-import a1 from './images/f12hole/a1.png';
-import a1flat from './images/f12hole/a1flat.png';
-import a1sharp from './images/f12hole/a1sharp.png';
-import a2 from './images/f12hole/a2.png';
-import a2flat from './images/f12hole/a2flat.png';
-import b1 from './images/f12hole/b1.png';
-import b1flat from './images/f12hole/b1flat.png';
-import b2 from './images/f12hole/b2.png';
-import c1 from './images/f12hole/c1.png';
-import c1flat from './images/f12hole/c1flat.png';
-import c1sharp from './images/f12hole/c1sharp.png';
-import d1 from './images/f12hole/d1.png';
-import d1flat from './images/f12hole/d1flat.png';
-import d1sharp from './images/f12hole/d1sharp.png';
-import e1 from './images/f12hole/e1.png';
-import e2 from './images/f12hole/e2.png';
-import e2flat from './images/f12hole/e2flat.png';
-import f1 from './images/f12hole/f1.png';
-import f1sharp from './images/f12hole/f1sharp.png';
-import f2 from './images/f12hole/f2.png';
-import f2sharp from './images/f12hole/f2sharp.png';
-import g1 from './images/f12hole/g1.png';
-import g1flat from './images/f12hole/g1flat.png';
-import g1sharp from './images/f12hole/g1sharp.png';
-import g2 from './images/f12hole/g2.png';
-import g2flat from './images/f12hole/g2flat.png';
-import g2sharp from './images/f12hole/g2sharp.png';
+import a4 from './images/f12hole/a4.png';
+import a4flat from './images/f12hole/a4flat.png';
+import a4sharp from './images/f12hole/a4sharp.png';
+import a5 from './images/f12hole/a5.png';
+import a5flat from './images/f12hole/a5flat.png';
+import b4 from './images/f12hole/b4.png';
+import b4flat from './images/f12hole/b4flat.png';
+import b5 from './images/f12hole/b5.png';
+import c5 from './images/f12hole/c5.png';
+import c5flat from './images/f12hole/c5flat.png';
+import c5sharp from './images/f12hole/c5sharp.png';
+import d5 from './images/f12hole/d5.png';
+import d5flat from './images/f12hole/d5flat.png';
+import d5sharp from './images/f12hole/d5sharp.png';
+import e4 from './images/f12hole/e4.png';
+import e5 from './images/f12hole/e5.png';
+import e5flat from './images/f12hole/e5flat.png';
+import f4 from './images/f12hole/f4.png';
+import f4sharp from './images/f12hole/f4sharp.png';
+import f5 from './images/f12hole/f5.png';
+import f5sharp from './images/f12hole/f5sharp.png';
+import g4 from './images/f12hole/g4.png';
+import g4flat from './images/f12hole/g4flat.png';
+import g4sharp from './images/f12hole/g4sharp.png';
+import g5 from './images/f12hole/g5.png';
+import g5flat from './images/f12hole/g5flat.png';
+import g5sharp from './images/f12hole/g5sharp.png';
 
 
 const notes = {
   f12hole : {
-    a1,
-    a1flat,
-    a1sharp,
-    a2,
-    a2flat,
-    b1,
-    b1flat,
-    b2,
-    c1,
-    c1flat,
-    c1sharp,
-    d1,
-    d1flat,
-    d1sharp,
-    e1,
-    e2,
-    e2flat,
-    f1,
-    f1sharp,
-    f2,
-    f2sharp,
-    g1,
-    g1flat,
-    g1sharp,
-    g2,
-    g2flat,
-    g2sharp,
+    a4,
+    a4flat,
+    a4sharp,
+    a5,
+    a5flat,
+    b4,
+    b4flat,
+    b5,
+    c5,
+    c5flat,
+    c5sharp,
+    d5,
+    d5flat,
+    d5sharp,
+    e4,
+    e5,
+    e5flat,
+    f4,
+    f4sharp,
+    f5,
+    f5sharp,
+    g4,
+    g4flat,
+    g4sharp,
+    g5,
+    g5flat,
+    g5sharp,
   }
 }
 
